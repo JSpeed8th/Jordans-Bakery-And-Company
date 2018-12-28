2a0015cbbdce6b7ff9e8c2584129a22b18f7b230
Jordan <jspeed8th@gmail.com>
2018-12-28 11:44:13 -0500

    added image_url column to Inventory table. Also, added images for cakes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,37 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 inventories = Inventory.create([
+  # CAKES
   {
     type_of_bake: 'cake',
     name: 'Charlotte Royale',
     description: 'Our decadent trifle can be served hot or cold. It is also referred to as an "icebox cake". Bread, sponge cake or biscuits/cookies are used to line a mold, which is then filled with a fruit puree or custard.',
     ingredients: '',
-    price: 52.99
+    price: 52.99,
+    image_url: 'cakes/charlotte-royale.jpg'
   },
   {
     type_of_bake: 'cake',
     name: 'Tiramisu Cake',
     description: 'Made of a tender vanilla cake soaked with coffee syrup, topped with creamy mascarpone frosting, and dusted with cocoa powder. Just like classic tiramisu in cake form!',
     ingredients: '',
-    price: 45.99
+    price: 45.99,
+    image_url: 'cakes/tiramisu.jpg'
   },
   {
     type_of_bake: 'cake',
     name: 'Hazelnut Dacquoise',
     description: 'Our french dessert contains layers of hazelnut meringue, hazelnut praline, chocolate ganache and a coffee custard filling. You will not be disapointed',
     ingredients: '',
-    price: 39.99
+    price: 39.99,
+    image_url: 'cakes/hazelnut-dacquoise.jpg'
   },
   {
     type_of_bake: 'cake',
     name: 'Prinsesstårta',
     description: 'A princess cake is a traditional Swedish layer cake or torte consisting of alternating layers of airy sponge cake, jam, pastry cream, and a thick-domed layer of whipped cream. This is topped by marzipan, giving the cake a smooth rounded top.',
     ingredients: '',
-    price: 59.99
+    price: 59.99,
+    image_url: 'cakes/prinsesstårta.jpg'
   }
   ])
