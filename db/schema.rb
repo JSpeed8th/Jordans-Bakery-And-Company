e2219b612ca18b865722ac89d71257d57830f5b3
Jordan <jspeed8th@gmail.com>
2019-01-07 17:42:42 -0500

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2019_01_01_221640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "phone_number"
+    t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
