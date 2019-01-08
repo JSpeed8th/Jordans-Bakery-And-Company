45b40b39d85f442fe6f2bc53483aea139904d440
Jordan <jspeed8th@gmail.com>
2019-01-07 19:58:11 -0500

    heroku

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-const edit = () => {
+var edit = () => {
 	document.querySelector('.profile').style.display = 'none';
 	document.querySelector('.edit-profile').style.display = 'block';
 	console.log('it worked')
